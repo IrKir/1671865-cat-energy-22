198357b8f4f3f887d6f643055111b5984d3a8331
Lisi4ka <kirich44@yandex.ru>
2021-04-24 23:59:26 +0500

    Создай автоматическую оптимизацию изображений

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,12 @@ const svgsprite = require("gulp-svg-sprite");
 const rename = require("gulp-rename");
 const sync = require("browser-sync").create();
 const htmlmin = require("gulp-htmlmin");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const del = require("del");
+const sync = require("browser-sync").create();
+const csso = require("postcss-csso");
 
 // Styles
 
@@ -41,6 +47,48 @@ const html = () => {
 
 exports.html = html;
 
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+    .pipe(terser())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+// Images
+
+const optimizeImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.images = copyImages;
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+exports.createWebp = createWebp;
 
 // Svg stack
 
